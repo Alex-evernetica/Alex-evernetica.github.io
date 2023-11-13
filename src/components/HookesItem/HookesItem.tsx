0aa77bf89a5aee6ed16aae7977e6610df836554a
Alex-evernetica <alexander.lizunov@evernetica.com>
2023-11-13 16:17:20 +0200

    padding fixed

--- src/components/HookesItem/HookesItem.tsx
+++ src/components/HookesItem/HookesItem.tsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import './HookesItem.scss'
-//
-// interface HookesItemProps {
-//     text: React.ReactNode[];
-//     image: string;
-//     imageClassname?: string;
-//     children?: React.ReactNode;
-// }
-//
-// const hookesItem: React.FC<HookesItemProps> = ({text, image, imageClassname, children}) => {
-//     return (
-//         <div className={'hookesItem'}>
-//             <div className={'hookesItem__text'}>{text}</div>
-//             {
-//                 image &&(
-//                     <img className={imageClassname} src={image} alt={'Hookes item image'}/>
-//                 )
-//             }
-//             {children}
-//         </div>
-//     );
-// }
-// export default hookesItem;
 import React from "react";
 import './HookesItem.scss'
 
